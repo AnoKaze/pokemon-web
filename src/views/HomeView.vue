dee37c3642c4d1afb8037f97aed3e613d889e2ba
AnoKaze <422365654@qq.com>
2022-12-13 01:14:46 +0800

    feat: 调整了TypeLabel的样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <type-label :type="type0" />
     <type-label :type="type1" />
+    <type-label :type="type0" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Type } from '@/types/type';
 
-const type0 = new Type(0, '一般');
 const type1 = new Type(1, '火');
+const type0 = new Type(2, '一般');
 </script>
