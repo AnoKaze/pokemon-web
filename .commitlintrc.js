b792346a65cb4c03d174118906137dd2a9aff57e
AnoKaze <422365654@qq.com>
2022-12-13 09:32:37 +0800

    chore: 修改了git工具包的脚本

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['init', 'build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
-    ],
-  },
 };
